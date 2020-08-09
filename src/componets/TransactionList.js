e3fcada8c9600d945023aae9c4a33e21b7e60e42
VKVERMA1122 <vaibhavkumarverma1@gmail.com>
2020-08-09 14:00:17 +0530

    3rd

--- src/componets/TransactionList.js
+++ src/componets/TransactionList.js
@@ -1,26 +1,17 @@
-import React, { useContext } from "react";
-import { GlobalContext } from "../context/Sate";
+import React, { useContext } from 'react';
+import { Transaction } from './Transaction';
+import { GlobalContext } from '../context/Sate';
 
-const TransactionList = () => {
+export const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
-  const sign = transactions.amount < 0 ? "-" : "+";
-  console.log("am", sign);
+
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        {transactions.map((transaction) => (
-          <li className="minus" key={transaction.id}>
-            {transaction.text}
-            <span>
-              {sign}
-              {Math.abs(transaction.amount)}
-            </span>
-            <button className="delete-btn">x</button>
-          </li>
-        ))}
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
       </ul>
     </>
-  );
-};
-export default TransactionList;
+  )
+}
+export default TransactionList
--- src/context/Sate.js
+++ src/context/Sate.js
@@ -45,3 +45,5 @@ export const GlobalProvider = ({ children }) => {
     </GlobalContext.Provider>
   );
 };
+
+
