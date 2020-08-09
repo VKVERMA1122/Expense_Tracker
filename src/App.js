b817ecdab0a3dd290e3574aa8c247464e82aac0b
VKVERMA1122 <vaibhavkumarverma1@gmail.com>
2020-08-09 13:18:43 +0530

    2nd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./styles.css";
-import Header from "../componets/Header";
-import Balance from "../componets/Balance";
-import IncomeExpence from "../componets/incomeExpence";
-import TransactionList from "../componets/TransactionList";
-import AddTransaction from "../componets/AddTransaction";
-import { GlobalProvider } from "../context/Sate";
+import Header from "./componets/Header";
+import Balance from "./componets/Balance";
+import IncomeExpence from "./componets/incomeExpence";
+import TransactionList from "./componets/TransactionList";
+import AddTransaction from "./componets/AddTransaction";
+import { GlobalProvider } from "./context/Sate";
 
 export default function App() {
   return (
